efc9cfcefdb62ff8798cbff07ef01deb3f2e5752
xarkamx <albertogmx91@gmail.com>
2021-11-06 23:58:18 -0600

    replace arrow

--- src/core/Calcs.js
+++ src/core/Calcs.js
@@ -1,26 +1,26 @@
 import { numberPadStart } from "./helpers";
 /* eslint eqeqeq: 0*/
 export class Calcs {
-  getCenter = dom => {
+  getCenter(dom) {
     const pos = dom.getBoundingClientRect();
     return {
       y: pos.y - pos.height / 2,
-      x: pos.x + pos.width / 2
+      x: pos.x + pos.width / 2,
     };
-  };
-  scaleTo = (current, maxVal = 10) => {
+  }
+  scaleTo(current, maxVal = 10) {
     let pow = Math.pow(maxVal, Math.log10(maxVal));
     return current / pow;
-  };
-  hipotenusa = (coord1, coord2) => {
+  }
+  hipotenusa(coord1, coord2) {
     let x = coord1.x - coord2.x;
     let y = coord1.y - coord2.y;
     return {
       hipotenusa: Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)),
       legY: y,
-      legX: x
+      legX: x,
     };
-  };
+  }
 }
 export class Time {
   miliseconds = 0;
@@ -78,7 +78,7 @@ export class Time {
     return {
       hours: numberPadStart(2, Math.floor(hours)),
       minutes: numberPadStart(2, Math.floor(minutes)),
-      seconds: numberPadStart(2, Math.round(seconds))
+      seconds: numberPadStart(2, Math.round(seconds)),
     };
   }
   /**
